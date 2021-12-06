877c25cf76a7f86605c30131ba40f80421ce4a1e
Callum Hemming <callumhemming@Amiras-MacBook-Pro.local>
2021-12-06 16:56:46 +0000

    complete

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import myCollection from "./collection.js";
 
-
+import chalk from "chalk";
 
 function describeItem(item) {
   let outPut = "";
 
     myCollection.forEach(function (params) { 
+     
 
         if (params.name === item && params.count == 1)  {
-            outPut =  `"I have a ${params.name} I like ${params.whatILike} about it`;
+            outPut =  `"I have a ${chalk.cyan(params.name)} I like ${chalk.green(params.whatILike)} about it`;
         }else if(params.name === item && params.count>1){
-          outPut =  `"I have ${params.count} ${params.name}s I like ${params.whatILike} about it`;
+          outPut =  `"I have ${chalk.yellow(params.count)} ${chalk.cyan(params.name)}s I like ${chalk.green(params.whatILike)} about it`;
         }
         
     })
@@ -22,12 +23,14 @@ function describeItem(item) {
 
 console.log(describeItem("School of Code hat"));
 
-  /*2c. Create a function called describeItem, which should take in an item as a parameter (the argument handed to this function would be an item from our collection). The function should console.log a message according to how many of the item you have.
+// Now let's bring in the power of NPM (Node Package Manager), a platform where people publish Node packages, which are bundles of software that you can use NPM to import into and use in your own projects. We'll use the `chalk` module to make our console.log messages a little more snazzy! 💅🌈✨
 
-  If there's only one of the item in your collection, it should log "I have a name. Here's what I like about it: whatILike". If you have more than one of it, the message should log "I have count names. Here's what I like about them: whatILike".
-  
-  For example, the result of calling the describeItem function with the first item in our collection would be: "I have a School of Code mug. Here's what I like about it: It has my cute pixel character on it!"
-  
-  Call your function below where you've defined it, handing in the first item in myCollection.
-  
-  Now run the file again (using node index.js);*/
+// 👉 4a. We've already initialised our project folder as an NPM project. That means we can use node modules other people have created in our project... Sweet. Makes this task easy 🙂
+
+// 👉 4b. Use the [chalk docs](https://www.npmjs.com/package/chalk) to find out how to install `chalk` to your project (check your dependencies in your `package.json` file if you want to check that you've installed it correctly). Follow the docs to see how you can now use chalk in `index.js`.
+
+// 👉 4c. Use `chalk` to do the following in your console.logs within `describeItem`:
+
+// - Make the name of each item in your collection cyan
+// - Make the count of each item in your collection yellow
+// - Make what you like about each item in your collection green
